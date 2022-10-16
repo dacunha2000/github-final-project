b5751d825845589f486b32fab291fa17e4f3348b
dacunha2000 <115627528+dacunha2000@users.noreply.github.com>
2022-10-16 17:10:35 +0200

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,11 @@
+3.	   #!/bin/bash
+4.	   # This script calculates simple interest given principal,
+5.	   # annual rate of interest and time period in years.
+6.	
+7.	   # Do not use this in production. Sample purpose only.
+8.	
+9.	   # Author: Upkar Lidder (IBM)
+10.	   # Additional Authors:
+11.	   # dacunha2000
+12.	
+13.	   # Input:
